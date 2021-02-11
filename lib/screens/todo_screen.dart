fcf1764b3f9b93628f559251b13306bf28e8b3cb
Noah Stinson <noahstjohn@ymail.com>
2021-02-11 16:34:11 -0600

    working on panel for folder/tasks

--- lib/screens/todo_screen.dart
+++ lib/screens/todo_screen.dart
@@ -1,4 +1,9 @@
+import 'package:creative2/model/Folder.dart';
+import 'package:creative2/model/ListItem.dart';
+import 'package:creative2/model/Task.dart';
+import 'package:creative2/model/ToDoList.dart';
 import 'package:flutter/material.dart';
+import 'dart:math' as math;
 
 class ToDoScreen extends StatefulWidget {
   static const routeName = '/toDoScreen';
@@ -10,13 +15,141 @@ class ToDoScreen extends StatefulWidget {
 }
 
 class _ToDoState extends State<ToDoScreen> {
+  ToDoList userList;
+  _ToDoController con;
+
+  @override
+  void initState() {
+    super.initState();
+    userList = ToDoList();
+    con = _ToDoController(this);
+  }
+
+  void render(func) {
+    setState(func);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('ToDo Screen'),
+        title: Text('User\'s To-Do List'),
+        actions: [
+          IconButton(icon: Icon(Icons.add), onPressed: null),
+        ],
       ),
-      body: Text('ToDo'),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Panel(
+              isFolder: false,
+              name: 'Task 1',
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _ToDoController {
+  _ToDoState state;
+
+  _ToDoController(this.state);
+}
+
+// custome widget for to do list panels
+class Panel extends StatefulWidget {
+  // final Widget child;
+  final String name;
+  final bool isFolder;
+
+  Panel({@required this.isFolder, this.name});
+
+  @override
+  State<StatefulWidget> createState() {
+    return _PanelState();
+  }
+}
+
+class _PanelState extends State<Panel> {
+  ListItem item;
+
+  @override
+  void initState() {
+    super.initState();
+    item = Folder(name: 'Task 1', id: 0);
+    item.isExpanded = true;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 300.0,
+      height: 50.0,
+      decoration: BoxDecoration(
+        color: Colors.grey[600],
+        borderRadius: BorderRadius.circular(7.0),
+      ),
+      child: Row(
+        children: [
+          SizedBox(
+            width: 15.0,
+          ),
+          Expanded(
+            flex: 20,
+            child: Text(
+              widget.name,
+              style: Theme.of(context).textTheme.headline1,
+            ),
+          ),
+          Expanded(
+            flex: 4,
+            child: item.isFolder
+                ? IconButton(
+                    icon: Icon(
+                      Icons.add,
+                      size: 36.0,
+                      color: Colors.white,
+                    ),
+                    onPressed: null,
+                  )
+                : SizedBox(),
+          ),
+          Expanded(
+            flex: 2,
+            child: item.isFolder
+                ? item.isExpanded
+                    ? Padding(
+                        padding: const EdgeInsets.fromLTRB(25.0, 0.0, 0.0, 25.0),
+                        child: IconButton(
+                            icon: Transform.rotate(
+                              angle: math.pi / 2,
+                              child: Icon(
+                                Icons.arrow_forward_ios_rounded,
+                                size: 30.0,
+                                color: Colors.white,
+                              ),
+                            ),
+                            onPressed: null),
+                      )
+                    : Padding(
+                        padding: const EdgeInsets.fromLTRB(0.0, 4.0, 0.0, 0.0),
+                        child: IconButton(
+                            icon: Icon(
+                              Icons.arrow_forward_ios_rounded,
+                              size: 30.0,
+                              color: Colors.white,
+                            ),
+                            onPressed: null),
+                      )
+                : SizedBox(),
+          ),
+          SizedBox(
+            width: 30.0,
+          )
+        ],
+      ),
     );
   }
 }
