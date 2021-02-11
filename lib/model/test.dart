88451108cd233ff5b67f7a1b55feca3e19893e2a
Noah Stinson <noahstjohn@ymail.com>
2021-02-10 18:41:34 -0600

    test code

--- lib/model/test.dart
+++ lib/model/test.dart
@@ -3,30 +3,34 @@ import 'package:creative2/model/ToDoList.dart';
 void main() {
   var list = ToDoList(name: 'My List');
 
-  list.addItem(isFolder: false, name: 'Task 1');
-  list.addItem(isFolder: true, name: 'Folder 1');
-  list.children[1].createItem(isFolder: true, name: 'Folder 1-1');
-  list.children[1].children[0].createItem(isFolder: false, name: 'Task 1-1-1');
-  list.children[1].children[0].createItem(isFolder: false, name: 'Task 1-1-2');
-  list.children[1].children[0].createItem(isFolder: false, name: 'Task 1-1-3');
-  list.children[0].createItem(isFolder: false, name: 'Task 1-1');
-  list.addItem(isFolder: false, name: 'Task 2');
-  list.addItem(isFolder: true, name: 'Folder 2');
-  list.children[3].createItem(isFolder: true, name: 'Folder 2-1');
-  list.children[3].createItem(isFolder: true, name: 'Folder 2-2');
-  list.children[3].createItem(isFolder: true, name: 'Folder 2-3');
-  list.children[3].createItem(isFolder: false, name: 'Task 2-1');
+  // Task 1
+  list.addItem(isFolder: false, addID: 0, name: 'Task 1');
 
-  list.children[3].children[1].createItem(isFolder: true, name: 'Folder 2-2-1');
-  list.children[3].children[1].children[0]
-      .createItem(isFolder: true, name: 'Folder 2-2-1-1');
-  list.children[3].children[1].children[0].children[0]
-      .createItem(isFolder: true, name: 'Folder 2-2-1-1-1');
+  // Folder 1
+  list.addItem(isFolder: true, addID: 0, name: 'Folder 1');
+  list.addItem(isFolder: true, addID: 2, name: 'Folder 1-1');
+  list.addItem(isFolder: false, addID: 3, name: 'Task 1-1-1');
+  list.addItem(isFolder: false, addID: 3, name: 'Task 1-1-2');
+  list.addItem(isFolder: false, addID: 3, name: 'Task 1-1-3');
+
+  // Task 2
+  list.addItem(isFolder: false, addID: 0, name: 'Task 2');
+
+  // Folder 2
+  list.addItem(isFolder: true, addID: 0, name: 'Folder 2');
+  list.addItem(isFolder: true, addID: 8, name: 'Folder 2-1');
+  list.addItem(isFolder: true, addID: 8, name: 'Folder 2-2');
+  list.addItem(isFolder: true, addID: 8, name: 'Folder 2-2');
+  list.addItem(isFolder: false, addID: 8, name: 'Task 2-1');
+
+  list.addItem(isFolder: true, addID: 10, name: 'Folder 2-2-1');
+  list.addItem(isFolder: true, addID: 13, name: 'Folder 2-2-1-1');
+  list.addItem(isFolder: true, addID: 14, name: 'Folder 2-2-1-1-1');
 
   print(list.toString());
 
-  list.deleteItem(deleteID: 4);
-  // list.deleteItem(deleteID: 10);
-  print('\n======================\n');
-  print(list.toString());
+  // list.deleteItem(deleteID: 4);
+  // // list.deleteItem(deleteID: 10);
+  // print('\n======================\n');
+  // print(list.toString());
 }
