7a3a89c35c868d14137c20e0f953928e4b430649
Noah Stinson <noahstinson19@gmail.com>
2021-02-13 15:12:21 -0600

    added global count for to do lists

--- lib/model/ToDoList.dart
+++ lib/model/ToDoList.dart
@@ -3,17 +3,21 @@ import 'package:creative2/model/ListItem.dart';
 import 'package:creative2/model/Task.dart';
 
 class ToDoList {
+  static int globalListCounter = 0;
+
   List<ListItem> children = [];
   String name;
   int id = 0;
+  int localID = 0;
   int idCount = 0;
 
   ToDoList({this.name}) {
     this.name = name;
+    this.id = globalListCounter++;
   }
 
   // provide if item will be folder, it's name, and the ID of what it's parent will be
-  void addItem({bool isFolder, String name, int addID}) {
+  void addItem({bool isFolder, String name, int addID = 0}) {
     idCount++;
 
     // if adding to root
@@ -30,7 +34,8 @@ class ToDoList {
     // if adding to sub-root
     for (int i = 0; i < children.length; i++) {
       if (children[i].isFolder) {
-        children[i].addItem(isFolder: isFolder, name: name, addID: addID, newID: idCount);
+        children[i].addItem(
+            isFolder: isFolder, name: name, addID: addID, newID: idCount);
       }
     }
   }
