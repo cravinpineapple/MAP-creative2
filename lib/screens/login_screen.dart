d8f8af31a836f56d187376a556188c6dc26e649c
Noah Stinson <noahstinson19@gmail.com>
2021-02-13 17:58:07 -0600

    login screen functioning

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,18 +1,21 @@
+import 'package:creative2/model/UserRecord.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+
+import 'home_screen.dart';
 
 class LoginScreen extends StatefulWidget {
   static const routeName = '/loginScreen';
 
   @override
   State<StatefulWidget> createState() {
-    return LoginScreenState();
+    return _LoginScreenState();
   }
 }
 
-class LoginScreenState extends State<LoginScreen> {
+class _LoginScreenState extends State<LoginScreen> {
   _Controller con;
-  GlobalKey<FormState> key = GlobalKey<FormState>();
+  GlobalKey<FormState> formKey = GlobalKey<FormState>();
   String error;
 
   @override
@@ -36,6 +39,7 @@ class LoginScreenState extends State<LoginScreen> {
             Row(
               children: [
                 SizedBox(width: 30.0),
+                // logo
                 Container(
                   width: 350.0,
                   height: 150.0,
@@ -68,54 +72,81 @@ class LoginScreenState extends State<LoginScreen> {
             SizedBox(height: 100.0),
             Row(
               children: [
-                SizedBox(width: 50.0),
-                Column(
-                  children: [
-                    Text(
-                      'Sign In',
-                      style: TextStyle(
-                        fontSize: 20.0,
-                        color: Colors.white,
-                        fontFamily: 'Roboto',
-                      ),
-                    ),
-                    SizedBox(
-                      width: 300.0,
-                      child: Theme(
-                        data: Theme.of(context)
-                            .copyWith(primaryColor: Colors.white),
-                        child: TextFormField(
-                          style: TextStyle(color: Colors.white),
-                          decoration: InputDecoration(
-                            icon: Icon(Icons.email),
-                            hintText: 'levi.ackerman@scouts.com',
-                          ),
-                          keyboardType: TextInputType.emailAddress,
-                          autocorrect: false,
-                          onSaved: null,
-                          validator: null,
+                SizedBox(width: 70.0),
+                Form(
+                  key: formKey,
+                  child: Column(
+                    children: [
+                      Text(
+                        'Sign In',
+                        style: TextStyle(
+                          fontSize: 20.0,
+                          color: Colors.white,
+                          fontFamily: 'Roboto',
                         ),
                       ),
-                    ),
-                    SizedBox(
-                      width: 300.0,
-                      child: Theme(
-                        data: Theme.of(context)
-                            .copyWith(primaryColor: Colors.white),
-                        child: TextFormField(
-                          style: TextStyle(color: Colors.white),
-                          decoration: InputDecoration(
-                            icon: Icon(Icons.security),
-                            hintText: 'password123',
+                      // Error message if inputted account information invalid
+                      error == null
+                          ? SizedBox()
+                          : Text(
+                              error,
+                              style: TextStyle(
+                                color: Colors.red[800],
+                                fontSize: 15.0,
+                              ),
+                            ),
+                      SizedBox(
+                        width: 260.0,
+                        child: Theme(
+                          data: Theme.of(context)
+                              .copyWith(primaryColor: Colors.white),
+                          child: TextFormField(
+                            style: TextStyle(color: Colors.white),
+                            decoration: InputDecoration(
+                              icon: Icon(Icons.email),
+                              hintText: 'levi.ackerman@scouts.com',
+                            ),
+                            keyboardType: TextInputType.emailAddress,
+                            autocorrect: false,
+                            onSaved: con.emailSave,
+                            validator: con.emailValidate,
                           ),
-                          keyboardType: TextInputType.emailAddress,
-                          autocorrect: false,
-                          onSaved: null,
-                          validator: null,
                         ),
                       ),
-                    ),
-                  ],
+                      SizedBox(
+                        width: 260.0,
+                        child: Theme(
+                          data: Theme.of(context)
+                              .copyWith(primaryColor: Colors.white),
+                          child: TextFormField(
+                            style: TextStyle(color: Colors.white),
+                            decoration: InputDecoration(
+                              icon: Icon(Icons.security),
+                              hintText: 'password123',
+                            ),
+                            keyboardType: TextInputType.emailAddress,
+                            autocorrect: false,
+                            onSaved: con.passwordSave,
+                            validator: con.passwordValidate,
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 20.0),
+                      RaisedButton(
+                        color: Colors.grey[600],
+                        onPressed: con.login,
+                        splashColor: Colors.white,
+                        child: Text(
+                          'Login',
+                          style: TextStyle(
+                            fontFamily: 'Roboto',
+                            fontSize: 20.0,
+                            color: Colors.white,
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
                 ),
               ],
             ),
@@ -127,7 +158,48 @@ class LoginScreenState extends State<LoginScreen> {
 }
 
 class _Controller {
-  LoginScreenState state;
+  _LoginScreenState state;
+  UserRecord userRecord = UserRecord();
 
   _Controller(this.state);
+
+  void emailSave(String value) {
+    userRecord.email = value;
+  }
+
+  String emailValidate(String value) {
+    if (value.contains('@') && value.contains('.')) return null;
+
+    return 'email invalid';
+  }
+
+  void passwordSave(String value) {
+    userRecord.password = value;
+  }
+
+  String passwordValidate(String value) {
+    if (value.length < 6) return 'password too short';
+
+    return null;
+  }
+
+  void login() {
+    // check if email / password are valid
+    state.render(() => state.error = null);
+    if (!state.formKey.currentState.validate()) return;
+
+    // email / password format is valid, now save information.
+    state.formKey.currentState.save();
+
+    var user = UserRecord.fakeDB.firstWhere(
+        (e) => e.email == userRecord.email && e.password == userRecord.password,
+        orElse: () => null);
+
+    if (user == null) {
+      state.render(() => state.error = 'email or password invalid');
+    } else {
+      state.render(() => state.error = null);
+      Navigator.pushNamed(state.context, HomeScreen.routeName, arguments: user);
+    }
+  }
 }
