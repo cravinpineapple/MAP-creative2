de114e8bde165ba5508234ef6727f1b16275c6c3
Noah Stinson <noahstinson19@gmail.com>
2021-02-14 13:20:03 -0600

    user account lists displayed

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -28,6 +28,7 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     userRecord = ModalRoute.of(context).settings.arguments;
+    List<Widget> builtList = con.buildList();
 
     return Scaffold(
       backgroundColor: Colors.grey[800],
@@ -35,11 +36,49 @@ class _HomeScreenState extends State<HomeScreen> {
         title: Text('Welcome, ${userRecord.firstName} ${userRecord.lastName}'),
         backgroundColor: Colors.grey[600],
       ),
-      body: Padding(
-        padding: const EdgeInsets.fromLTRB(20.0, 20.0, 0.0, 0.0),
-        child: Column(
-          children: [
-            Container(
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.fromLTRB(20.0, 20.0, 20.0, 0.0),
+          child: Column(
+            children: builtList.length != 0
+                ? builtList
+                : [
+                    Padding(
+                      padding: const EdgeInsets.only(
+                        top: 240.0,
+                        left: 22.0,
+                        // right: 35.0,
+                      ),
+                      child: Text(
+                        'You currently have no to-do lists.\nClick the + to create one!',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          color: Colors.grey[400],
+                          fontSize: 23.0,
+                        ),
+                      ),
+                    ),
+                  ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class _Controller {
+  _HomeScreenState state;
+
+  _Controller(this.state);
+
+  List<Widget> buildList() {
+    if (state.userRecord.toDoLists == null) return [];
+
+    return state.userRecord.toDoLists
+        .map(
+          (e) => Padding(
+            padding: const EdgeInsets.only(bottom: 13.0),
+            child: Container(
               width: 370.0,
               height: 75.0,
               child: RaisedButton(
@@ -52,24 +91,13 @@ class _HomeScreenState extends State<HomeScreen> {
                 color: Colors.grey[300],
                 onPressed: () {},
                 child: Text(
-                  'YoYoYoYoYo',
+                  e.name,
                   style: TextStyle(fontSize: 40.0),
                 ),
               ),
             ),
-          ],
-        ),
-      ),
-    );
+          ),
+        )
+        .toList();
   }
-}
-
-class _Controller {
-  _HomeScreenState state;
-
-  _Controller(this.state);
-
-  // List buildLists() {
-
-  // }
 }
